434cc607b7596e41849f40875d0cdc9d8e22b193
clashino <tkdoci890@gmail.com>
2026-07-06 03:42:25 +0330

    Create start handler

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer(
+        "👋 سلام، به ربات Clashino خوش آمدید."
+    )
